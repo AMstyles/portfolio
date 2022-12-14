8d730834525a197551731f4a6362a1a19037dac0
Arinaho Muleba <arinaomuleluz@gmail.com>
2022-12-14 09:56:07 +0200

    animations better

---
+++ src/components/games/treecounter.js
@@ -0,0 +1,24 @@
+
+import { useRive, useStateMachineInput } from "rive-react";
+import './treecounter.scss'
+
+function Counter() {
+    const { rive, RiveComponent } = useRive({
+        src: 'sign_up_now.riv',
+        autoplay: true,
+        stateMachines: 'State Machine 1'
+
+    })
+
+    return (<div className="container">
+        <div className="animation">
+
+        </div>
+        <div className="content">
+
+        </div>
+
+    </div>)
+}
+
+export default Counter;
