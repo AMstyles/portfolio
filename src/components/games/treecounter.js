7470e9ae5193c3ac4e1b7f592ebcd8732996653e
Arinaho Muleba <arinaomuleluz@gmail.com>
2022-12-19 15:32:10 +0200

    tree counter

--- src/components/games/treecounter.js
+++ src/components/games/treecounter.js
@@ -6,6 +6,8 @@ function Counter() {
 
     const initialheight = 0;
     const STATE_MACHINE_NAME = 'State Machine 1';
+    const INTERVAL = 30;
+    let isForward = true;
 
     const [height, setHeight] = useState(initialheight)
 
@@ -27,39 +29,91 @@ function Counter() {
         else {
             setHeight(height + 1);
             treeHeight.value = height;
-
         }
 
     }
 
+    function autoGrow() {
+
+        var myInterval = setInterval(() => {
+            if (treeHeight.value < 100) {
+                treeHeight.value = treeHeight.value + 1;
+                console.log(treeHeight.value)
+                setHeight(treeHeight.value)
+
+            }
+            else {
+
+                setHeight(100);
+                clearInterval(myInterval);
+                return;
+            }
+
+        }
+            , INTERVAL);
+
+
+    }
+
+    function autoShrink() {
+        let myInterval = setInterval(() => {
+            if (treeHeight.value > 0) {
+                treeHeight.value = treeHeight.value - 1;
+                console.log(treeHeight.value)
+                setHeight(treeHeight.value)
+
+            }
+            else {
+
+                setHeight(0);
+                clearInterval(myInterval);
+                return;
+            }
+
+        }
+            , INTERVAL);
+
+    }
+
+    function autoplay() {
+        if (height < 100) {
+            autoGrow();
+        }
+        else {
+            autoShrink();
+        }
+    }
+
+
+
 
 
     return (<div className="container">
-        <div className="animation" >
+        <div className="animation" onClick={autoplay}>
 
             <RiveComponent />
 
         </div>
+
         <div className="content">
             <div className="texts">
-                <h1>REACTIVE COUNTER</h1>
-                <h4>This is a counter to simulate a reactive website, have fun!</h4>
-                <p className="par">Use the bottons to change the age of the tree</p>
-                <h1>{height}</h1>
+                <h1 className="header">REACTIVE COUNTER</h1>
 
+                <h2 className="sub-header">This is a counter to simulate a reactive website, have fun!</h2>
 
+                <h className="height" >{height}</h>
+                <p className="par"> <span className="protip">Protip </span>: click on the tree to animate its growth</p>
             </div>
-
-
 
             <div className="actions">
 
-                <button className="autoShrink" >Shrink</button>
+                {/* <button className="autoShrink" >Shrink</button> */}
 
                 <button className="btnd" onClick={() => { handleHeight('d') }}>-</button>
                 <button className="btni" onMouseDown={() => { handleHeight('i') }} >+</button>
 
-                <button className="autoGrow" >Auto Grow</button>
+
+                {/* <button className="autoGrow" >Auto Grow</button> */}
             </div>
 
 
